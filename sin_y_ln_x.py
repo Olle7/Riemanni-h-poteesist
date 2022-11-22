8b8ccacb3ed91cb81efd1aaba1fe662e905adbe9
Olger Männik <Olgerm@protonmail.com>
2022-11-23 01:00:36 +0200

    asenda~in  nulllõpuga zetafunktsiooni vormis sinln'i selle taylori reaga.

--- sin_y_ln_x.py
+++ sin_y_ln_x.py
@@ -23,7 +23,6 @@ sin(x)=\sum_{k=0}^\infty((-1)^k*x^{2*k+1}*\Gamma(2*k+2)^{-1})\\
         #    s2+=(2*k2+1)**-1*(x-1)**(2*k2+1)*(x+1)**(-2*k2-1)
         s1+=(-1)**k1*x**(2*k1+1)*gamma(2*k1+2)**-1
     return s1
-
 def sin_x_maclaurin_optimeeritud(x,M=85):
     s1=0
     for k1 in range(M):
@@ -32,7 +31,6 @@ def sin_x_maclaurin_optimeeritud(x,M=85):
         #    s2+=(2*k2+1)**-1*(x-1)**(2*k2+1)*(x+1)**(-2*k2-1)
         s1+=(-1)**k1*x**(2*k1+1)*factorial(2*k1+1)**-1
     return s1
-
 def sin_y_ln_x_maclaurin_optimeeritud(x, y, M1=10000,M2=100000):
     s = 0
     try:
@@ -49,7 +47,6 @@ def sin_y_ln_x_maclaurin_optimeeritud(x, y, M1=10000,M2=100000):
     except OverflowError:
         pass
     return s
-
 def sin_y_ln_x_maclaurin_3(x,y,M1=10000,M2=100000):
     latex = r"""$\forall_x(x>0 \to\\
     sin(y*ln(x))=\sum_{k_1=0}^\infty(( \sum_{{k_2}=0}^\infty((2*k_2+1)^{-1}*(x-1)^{2*k_2+1}*(x+1)^{-2*k_2-1}) )^{2*k_1+1}*y^{2*k_1+1}*factorial(2*k_1+1)^{-1}*2^{2*k_1+1}(-1)^{k_1})\\
@@ -70,7 +67,6 @@ def sin_y_ln_x_maclaurin_3(x,y,M1=10000,M2=100000):
     except OverflowError:
         pass
     return s1
-
 def sin_y_ln_x_multinomial_1(x,y,M=6):##TOIMIB
     latex=r"""$\forall_x(x>0 \to\\
 sin(y*ln(x))=lim_{M \to \infty}(\sum_{k_1=0}^{M-1}(\sum_{k_2=0}^{(2*k_1+2)^M-1}(\\
@@ -95,7 +91,6 @@ sin(y*ln(x))=lim_{M \to \infty}(\sum_{k_1=0}^{M-1}(\sum_{k_2=0}^{(2*k_1+2)^M-1}(
             s2+=p3
         s1+=s2*y**(2*k1+1)*2**(2*k1+1)*(-1)**k1
     return s1
-
 def sin_y_ln_x_multinomial(x,y,M=6):
     latex=r"""$\forall_x(x>0 \to sin(y*ln(x))=\\
 lim_{M \to \infty}(\sum_{k_1=0}^{M-1}(y^{2*k_1+1}*2^{2*k_1+1}*(-1)^{k_1}*\sum_{k_2=0}^{(2*k_1+2)^M-1}(\\
@@ -130,7 +125,6 @@ lim_{M \to \infty}(\sum_{k_1=0}^{M-1}(y^{2*k_1+1}*2^{2*k_1+1}*(-1)^{k_1}*\sum_{k
             s2+=p
         s1+=s2*y**(2*k1+1)*2**(2*k1+1)*(-1)**(k1)
     return s1
-
 def sin_y_ln_x_multinomial_2(x,y,M=6):
     latex=r"""$\forall_x(x>0 \to sin(y*ln(x))=\\
 -lim_{M \to \infty}(\sum_{k_1=0}^{(2*M)^M-1}(\\
@@ -167,13 +161,106 @@ def sin_y_ln_x_multinomial_2(x,y,M=6):
             p*=factorial((k1//(2*M)**k2)%(2*M))**(-1)*(2*k2+1)**(-((k1//(2*M)**k2)%(2*M)))
         s1+=p
     return s1
+def cos_y_ln_x_multinomial_2(x,y,M=6):
+    latex=r"""$\forall_x(x>0 \to sin(y*ln(x))=\\
+lim_{M \to \infty}(\sum_{k_1=0}^{(2*M)^M-1}(\\
+(\sum_{k_2=0}^{M-1}((floor(k_1/(2*M-1)^{k_2})\%(2*M-1))+1)\%2)*\\
+(\sum_{k_2=0}^{M-1}(floor(k_1/(2*M-1)^{k_2})\%(2*M-1))<2*M-1)*\\%sulgudega 0
+(y*2)^{\sum_{k_2=0}^{M-1}(floor(k_1/(2*M-1)^{k_2})\%(2*M-1))}*\\
+(-1)^{\sum_{k_2=0}^{M-1}(floor(k_1/(2*M-1)^{k_2})\%(2*M-1))/2}*\\
+(x-1)^{ \sum_{k_2=0}^{M-1}((2*k_2+1)*(floor(k_1/(2*M-1)^{k_2})\%(2*M-1)))}*\\
+(x+1)^{-\sum_{k_2=0}^{M-1}((2*k_2+1)*(floor(k_1/(2*M-1)^{k_2})\%(2*M-1)))}*\\
+\Pi_{k_2=0}^{M-1}(factorial(floor(k_1/(2*M-1)^{k_2})\%(2*M-1))^{-1}*(2*k_2+1)^{-floor(k_1/(2*M-1)^{k_2})\%(2*M-1)} )\\
+))/2\\%k2-summa ja lim'i lopusulg sel real.
+)%forall lopusulg$"""
+    s1=0
+    for k1 in range((2*M)**M):
+        p=1
+        s2=0
+        for k2 in range(M):
+            s2+=(k1//(2*M-1)**k2)%(2*M-1)
+        p*=(s2+1)%2
+        p*=s2<2*M-1
+        if p==0:
+            continue
+        p*=(y*2)**s2
+        #print("s2:",s2)
+        p*=(-1)**(s2/2)
 
+        s2=0
+        for k2 in range(M):
+            s2+=(2*k2+1)*((k1//(2*M-1)**k2)%(2*M-1))
+        p*=(x-1)**s2
+        p*=(x+1)**-s2
+
+        for k2 in range(M):
+            p*=factorial((k1//(2*M-1)**k2)%(2*M-1))**(-1)*(2*k2+1)**(-((k1//(2*M-1)**k2)%(2*M-1)))
+        s1+=p
+    return s1/2
+def cos_y_ln_x_multinomial_4(x,y,M=6):
+    latex=r"""$\forall_x(x>0 \to sin(y*ln(x))=\\
+lim_{M \to \infty}(\sum_{k_1=0}^{(2*M)^M-1}(\\
+(\sum_{k_2=0}^{M-1}((floor(k_1/(2*M-1)^{k_2})\%(2*M-1))+1)\%2)*\\
+(y*2)^{\sum_{k_2=0}^{M-1}(floor(k_1/(2*M-1)^{k_2})\%(2*M-1))}*\\
+(-1)^{\sum_{k_2=0}^{M-1}(floor(k_1/(2*M-1)^{k_2})\%(2*M-1))/2}*\\
+(x-1)^{ \sum_{k_2=0}^{M-1}((2*k_2+1)*(floor(k_1/(2*M-1)^{k_2})\%(2*M-1)))}*\\
+(x+1)^{-\sum_{k_2=0}^{M-1}((2*k_2+1)*(floor(k_1/(2*M-1)^{k_2})\%(2*M-1)))}*\\
+\Pi_{k_2=0}^{M-1}(factorial(floor(k_1/(2*M-1)^{k_2})\%(2*M-1))^{-1}*(2*k_2+1)^{-floor(k_1/(2*M-1)^{k_2})\%(2*M-1)} )\\
+))/2\\%k2-summa ja lim'i lopusulg sel real.
+)%forall lopusulg$"""
+    s1=0
+    for k1 in range((2*M)**M):
+        p=1
+        s2=0
+        for k2 in range(M):
+            s2+=(k1//(2*M-1)**k2)%(2*M-1)
+        p*=(s2+1)%2
+        if p==0:
+            continue
+        p*=(y*2)**s2
+        #print("s2:",s2)
+        p*=(-1)**(s2/2)
+
+        s2=0
+        for k2 in range(M):
+            s2+=(2*k2+1)*((k1//(2*M-1)**k2)%(2*M-1))
+        p*=(x-1)**s2
+        p*=(x+1)**-s2
+
+        for k2 in range(M):
+            p*=factorial((k1//(2*M-1)**k2)%(2*M-1))**(-1)*(2*k2+1)**(-((k1//(2*M-1)**k2)%(2*M-1)))
+        s1+=p
+    return s1/2
+def cos_y_ln_x_multinomial_5(x,y,M=6):
+    s1=0
+    for k1 in range((2*M)**M):
+        p=1
+        s2=0
+        for k2 in range(M):
+            s2+=(k1//(2*M)**k2)%(2*M)
+        p*=(s2+1)%2
+        if p==0:
+            continue
+        p*=(y*2)**s2
+        #print("s2:",s2)
+        p*=(-1)**(s2/2)
+
+        s2=0
+        for k2 in range(M):
+            s2+=(2*k2+1)*((k1//(2*M)**k2)%(2*M))
+        p*=(x-1)**s2
+        p*=(x+1)**-s2
+
+        for k2 in range(M):
+            p*=factorial((k1//(2*M)**k2)%(2*M))**(-1)*(2*k2+1)**(-((k1//(2*M)**k2)%(2*M)))
+        s1+=p
+    return s1
 def sin_y_ln_x_multinomial_4(x,y,M=6):
     latex=r"""$\forall_x(x>0 \to sin(y*ln(x))=\\
--lim_{M \to \infty}(\sum_{k_1=0}^{(2*M)^M-1}(\\
+lim_{M \to \infty}(\sum_{k_1=0}^{(2*M)^M-1}(\\
 (\sum_{k_2=0}^{M-1}(floor(k_1/(2*M)^{k_2})\%(2*M))\%2)*\\
 (y*2)^{\sum_{k_2=0}^{M-1}(floor(k_1/(2*M)^{k_2})\%(2*M))}*\\
-(-1)^{(\sum_{k_2=0}^{M-1}(floor(k_1/(2*M)^{k_2})\%(2*M))-1)/2}*\\
+(-1)^{(\sum_{k_2=0}^{M-1}(floor(k_1/(2*M)^{k_2})\%(2*M))+3)/2}*\\
 (x-1)^{ \sum_{k_2=0}^{M-1}((2*k_2+1)*(floor(k_1/(2*M)^{k_2})\%(2*M)))}*\\
 (x+1)^{-\sum_{k_2=0}^{M-1}((2*k_2+1)*(floor(k_1/(2*M)^{k_2})\%(2*M)))}*\\
 \Pi_{k_2=0}^{M-1}(factorial(floor(k_1/(2*M)^{k_2})\%(2*M))^{-1}*(2*k_2+1)^{-floor(k_1/(2*M)^{k_2})\%(2*M)} )\\
@@ -191,7 +278,7 @@ def sin_y_ln_x_multinomial_4(x,y,M=6):
             continue
         p*=(y*2)**s2
         #print("s2:",s2)
-        p*=(-1)**((s2-1)/2)
+        p*=(-1)**((s2+3)/2)
 
         s2=0
         for k2 in range(M):
@@ -203,7 +290,41 @@ def sin_y_ln_x_multinomial_4(x,y,M=6):
             p*=factorial((k1//(2*M)**k2)%(2*M))**(-1)*(2*k2+1)**(-((k1//(2*M)**k2)%(2*M)))
         s1+=p
     return s1
+def cos_y_ln_x_multinomial_4_2(x,y,M=6):
+    latex=r"""$\forall_x(x>0 \to cos(y*ln(x))=\\
+lim_{M \to \infty}(\sum_{k_1=0}^{(2*M)^M-1}(\\
+(\sum_{k_2=0}^{M-1}(floor(k_1/(2*M)^{k_2})\%(2*M))\%2)*\\
+(y*2)^{\sum_{k_2=0}^{M-1}(floor(k_1/(2*M)^{k_2})\%(2*M))}*\\
+(-1)^{\sum_{k_2=0}^{M-1}(floor(k_1/(2*M)^{k_2})\%(2*M))/2}*\\
+(x-1)^{ \sum_{k_2=0}^{M-1}((2*k_2+1)*(floor(k_1/(2*M)^{k_2})\%(2*M)))}*\\
+(x+1)^{-\sum_{k_2=0}^{M-1}((2*k_2+1)*(floor(k_1/(2*M)^{k_2})\%(2*M)))}*\\
+\Pi_{k_2=0}^{M-1}(factorial(floor(k_1/(2*M)^{k_2})\%(2*M))^{-1}*(2*k_2+1)^{-floor(k_1/(2*M)^{k_2})\%(2*M)} )\\
+))\\%k2-summa ja lim'i lopusulg sel real.
+)%forall lopusulg$"""
+    s1=0
+    for k1 in range((2*M)**M):
+        p=1
+        s2=0
+        for k2 in range(M):
+            s2+=(k1//(2*M)**k2)%(2*M)
+        p*=(s2+1)%2
+        #p*=s2<2*M
+        if p==0:
+            continue
+        p*=(y*2)**s2
+        #print("s2:",s2)
+        p*=(-1)**(s2/2)
 
+        s2=0
+        for k2 in range(M):
+            s2+=(2*k2+1)* ((k1//(2*M)**k2)%(2*M))
+        p*=(x-1)**s2
+        p*=(x+1)**-s2
+
+        for k2 in range(M):
+            p*=factorial((k1//(2*M)**k2)%(2*M))**(-1)*(2*k2+1)**(-((k1//(2*M)**k2)%(2*M)))
+        s1+=p
+    return s1
 def sin_y_ln_x_multinomial_3(x,y,M=6):
     latex=r"""$\forall_x(x>0 \to sin(y*ln(x))=\\
 -lim_{M \to \infty}(\sum_{k_1=0}^{(2*M)^M-1}(\\
@@ -240,8 +361,6 @@ def sin_y_ln_x_multinomial_3(x,y,M=6):
             p*=factorial((k1//(M)**k2)%(M))**(-1)*(2*k2+1)**(-((k1//(M)**k2)%(M)))
         s1+=p
     return s1
-
-
 def cos_y_ln_x_multinomial(x,y,M=6):
     latex=r"""$\forall_x(x>0 \to cos(y*ln(x))=\\
 lim_{M \to \infty}(\sum_{k_1=0}^{M-1}(y^{2*k_1}*2^{2*k_1}*(-1)^{k_1}*\sum_{k_2=0}^{(2*k_1+1)^M-1}(\\
@@ -276,7 +395,6 @@ lim_{M \to \infty}(\sum_{k_1=0}^{M-1}(y^{2*k_1}*2^{2*k_1}*(-1)^{k_1}*\sum_{k_2=0
             s2+=p
         s1+=s2*y**(2*k1)*2**(2*k1)*(-1)**(k1)
     return s1
-
 def sin_y_ln_x_multinomial_optimeeritud(x, y, M=7):
     latex=r"""$\forall_x(x>0 \to sin(y*ln(x))=\\
 lim_{M \to \infty}(\sum_{k_1=0}^M(y^{2*k_1+1}*2^{2*k_1+1}*(-1)^{k_1}*\sum_{k_2=0}^{(2*k_1+1)^M}(\\
@@ -311,15 +429,17 @@ lim_{M \to \infty}(\sum_{k_1=0}^M(y^{2*k_1+1}*2^{2*k_1+1}*(-1)^{k_1}*\sum_{k_2=0
         s1+=s2*y**(2*k1+1)*2**(2*k1+1)*(-1)**(k1)
     return s1
 
+
+
 def kontrolli_sin_y_ln_x(sin_y_ln_x_maclaurin_funktsioon):
-    for x_y in [(2.845902,1),(1,2),(4,2),(2,3),(0.234,1),(3.232,0.3),(e**pi,3),]:
+    for x_y in [(2.845902,1),(1,2),(4,2),(2,3),(0.234,1),(3.232,0.3),(e**pi,3),(0.4242,0.3421),(1.1352,0.724241)]:
         x,y=x_y[0],x_y[1]
         õige=sin(y*log(x))
         vastus=sin_y_ln_x_maclaurin_funktsioon(x,y)
         print(õige-vastus,"vastus:",vastus,"; õige:",õige,"; x:",x,"; y:",y)
 
 def kontrolli_cos_y_ln_x(sin_y_ln_x_maclaurin_funktsioon):
-    for x_y in [(2.845902,1),(1,2),(4,2),(2,3),(0.234,1),(3.232,0.3),(e**pi,3),]:
+    for x_y in [(2.845902,1),(1,2),(4,2),(2,3),(0.234,1),(3.232,0.3),(e**pi,3),(0.4242,0.3421),(1.1352,0.724241)]:
         x,y=x_y[0],x_y[1]
         õige=cos(y*log(x))
         vastus=sin_y_ln_x_maclaurin_funktsioon(x,y)
@@ -360,5 +480,5 @@ ln(x)=2*\sum_{k=0}^\infty((2*k+1)^{-1}*(x-1)^{2*k+1}*(x+1)^{-2*k-1})\\
 #print(sin_y_ln_x_multinomial_1(0.5,0.5,6),sin_y_ln_x_multinomial(0.5,0.5,6))
 
 #print(ln_maclaurin(e**4.2642))
-kontrolli_sin_y_ln_x(sin_y_ln_x_multinomial_2)
+kontrolli_cos_y_ln_x(cos_y_ln_x_multinomial_4_2)
 #kontrolli_sin_x(sin_x_maclaurin_optimeeritud)
